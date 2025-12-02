9c297b20849d5c5785a9713e5f67873da1fb4ac0
veronica-ivanova <veronica.ivanova27@gmail.com>
2025-12-02 20:48:49 +0300

    feat: add footer component and media queries

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Navbar } from "./components/navbar/navbar";
 import { Routes, Route } from "react-router-dom";
 import  { Home }  from "./pages/home/home";
 import  { Coin } from "./pages/coin/coin";
+import { Footer } from "./components/footer/footer";
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
         <Route path="/" element={<Home/>}/>
         <Route path="/coin/:coinId" element={<Coin/>}/>
       </Routes>
+      <Footer/>
     </div>
   );
 };
---
+++ src/components/footer/footer.jsx
@@ -0,0 +1,9 @@
+import './footer.css'
+
+export const Footer = () => {
+  return (
+    <div className="footer">
+      <p>Copyright @ 2024, Cryptoplace - All Right Reserved.</p>
+    </div>
+  );
+};
