9368997c7d0d63f453b514ec6a64d4b26d82da10
veronica-ivanova <veronica.ivanova27@gmail.com>
2025-12-02 21:16:34 +0300

    feat: add search functionality

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -6,6 +6,22 @@ export const Home = () => {
 
   const {allCoin, currency} = useContext(CoinContext);
   const [displayCoin, setDisplayCoin] = useState([]);
+  const [input, setInput] = useState('');
+ 
+  const inputHandler = (event) => {
+    setInput(event.target.value);
+    if (event.target.value === ""){
+      setDisplayCoin(allCoin);
+    }
+  }
+
+  const searchHandler = async (event)=> {
+    event.preventDefault();
+    const coins = await allCoin.filter((item)=>{
+      return item.name.toLowerCase().includes(input.toLowerCase())
+    })
+    setDisplayCoin(coins);
+  }
 
   useEffect(()=>{
     setDisplayCoin(allCoin);
@@ -18,8 +34,14 @@ export const Home = () => {
       <p>Welcome to the world's largest cryptocurrency marketplace. 
         Sign up to explore more about cryptos.
       </p>
-      <form>
-        <input type="text" placeholder='Search crypto..'/>
+      <form onSubmit={searchHandler}>
+        <input onChange={inputHandler} list='coinlist' value={input} type="text" placeholder='Search crypto..'
+        required/>
+
+        <datalist id='coinlist'>
+          {allCoin.map((item, index)=>(<option key={index} value={item.name}/>))}
+        </datalist>
+
         <button type="submit">Search</button>
       </form>
      </div>
